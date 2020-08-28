f1d2c5caed4a8af6cf3aef1aee9b35db0e5a9d83
Reddy <Venu.Duggireddy.fp@perspecta.com>
2020-08-28 16:21:22 -0400

    added config and env vars

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-
+import {CoreModule} from '@nest-ql/core'
 @Module({
-  imports: [],
+  imports: [CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
